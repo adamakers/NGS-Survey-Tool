a7c643db98341409851d30c535b5d36a8bbf5663
Adam Akers <adamrakers@gmail.com>
2025-08-15 19:21:42 -0600

    github commit

--- ngs_tool.py
+++ ngs_tool.py
@@ -13,33 +13,31 @@ api_url = 'https://geodesy.noaa.gov/api/nde/radial?lat=38.954775&lon=-104.885727
 response = requests.get(api_url)
 
 if response.status_code == 200:
-  data = response.json()
-  print('Data has been recieved')
-  ngs_monuments = data;
-  print(ngs_monuments)
+    data = response.json()
+    print('Data has been recieved')
+    ngs_monuments = data;
+    print(ngs_monuments)
 else:
-  print(f'Error downloading data.  See error: {response.status_code}')
-
+    print(f'Error downloading data.  See error: {response.status_code}')
 
 for mon in ngs_monuments:
-  attributes = {
-    'pid': mon['pid'],
-    'name': mon['name'],
-    'stCounty': mon['stCounty'],
-    'lat': mon['lat'],
-    'lon': mon['lon'],
-    'vertDatum': mon['vertDatum'],
-    'vertSource': mon['vertSource'],
-    'posSource': mon['posSource'],
-    'condition': mon['condition'],
-    'setting': mon['setting']
-  }
-  
-  markers += to_kml.create_marker(attributes)
+    attributes = {
+        'pid': mon['pid'],
+        'name': mon['name'],
+        'stCounty': mon['stCounty'],
+        'lat': mon['lat'],
+        'lon': mon['lon'],
+        'vertDatum': mon['vertDatum'],
+        'vertSource': mon['vertSource'],
+        'posSource': mon['posSource'],
+        'condition': mon['condition'],
+        'setting': mon['setting']
+    }
 
+    markers += to_kml.create_marker(attributes)
 
 with open('test5.kml', 'w') as f:
-  f.write(to_kml.template(markers))
+    f.write(to_kml.template(markers))
 
 
 #information that is needed
@@ -61,4 +59,6 @@ with open('test5.kml', 'w') as f:
 #### enter user queries for lat lon radius
 #### different symbols for different types of desireable points
 #### Add URL to point
+#### add input to determine what level of accuracy
+    ####
 
--- to_kml.py
+++ to_kml.py
@@ -1,68 +1,60 @@
+# create a Style element to create an id of what an icon should be.  reference that ID in the placemark.  similar to HTML and css
+
 # creates the KML file
-def template(markers): 
+def template(markers):
   return f'''<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
 <Document>
-	<name>test5.kml</name>
-	<StyleMap id="sn_placemark_circle">
-		<Pair>
-			<key>normal</key>
-			<styleUrl>#sn_placemark_circle</styleUrl>
-		</Pair>
-		<Pair>
-			<key>highlight</key>
-			<styleUrl>#sn_placemark_circle</styleUrl>
-		</Pair>
-	</StyleMap>
-	<Style id="sn_placemark_circle">
-		<IconStyle>
-			<scale>1.1</scale>
-			<Icon>
-				<href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href>
-			</Icon>
-			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
-		</IconStyle>
-	</Style>
-	<Style id="sn_placemark_circle">
-		<IconStyle>
-			<scale>1.3</scale>
-			<Icon>
-				<href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href>
-			</Icon>
-			<hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/>
-		</IconStyle>
-	</Style>
-  f{markers}
+    <Style id="redIcon">
+        <IconStyle>
+            <color>ff0000</color>
+            <colorMode>normal</colorMode>
+            <scale>5</scale>
+            <Icon>
+                <href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href>
+            </Icon>
+        </IconStyle>
+    </Style>
+    <name>test5.kml</name>
+    {markers}
 </Document>
 </kml>
 '''
 
 #creates a single marker
 def create_marker(attributes):
-  return f'''
-    <Placemark>
-      <name>{attributes['pid']}</name>
-      <description>
-        stCounty: {attributes['stCounty']}
-        vertDatum: {attributes['vertDatum']}
-        vertSource: {attributes['vertSource']}
-        condition: {attributes['condition']}
-        setting: {attributes['setting']}
-      </description>
-      <LookAt>
-        <longitude>{attributes['lon']}</longitude>
-        <latitude>{attributes['lat']}</latitude>
-        <altitude>0</altitude>
-        <heading>-0.03339063639233215</heading>
-        <tilt>0</tilt>
-        <range>23947.66089891575</range>
-        <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
-      </LookAt>
-      <styleUrl>#sn_placemark_circle</styleUrl>
-      <Point>
-        <gx:drawOrder>1</gx:drawOrder>
-        <coordinates>{attributes['lon']},{attributes['lat']},0</coordinates>
-      </Point>
-      <atom:link rel="app" href="https://www.google.com/earth/about/versions/#earth-pro" title="Google Earth Pro 7.3.6.10201"></atom:link>
-    </Placemark>
-  '''
+    return f'''
+        <Placemark>
+            <Style>
+                <IconStyle>
+                    <color>ff0000</color>
+                    <colorMode>normal</colorMode>
+                    <scale>5</scale>
+                    <Icon>
+                        <href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href>
+                    </Icon>
+                </IconStyle>
+            </Style>
+            <name>{attributes['pid']}</name>
+            <description>
+                stCounty: {attributes['stCounty']}
+                vertDatum: {attributes['vertDatum']}
+                vertSource: {attributes['vertSource']}
+                condition: {attributes['condition']}
+                setting: {attributes['setting']}
+            </description>
+            <LookAt>
+                <longitude>{attributes['lon']}</longitude>
+                <latitude>{attributes['lat']}</latitude>
+                <altitude>0</altitude>
+                <heading>0.0</heading>
+                <tilt>0</tilt>
+                <range>23947.66089891575</range>
+                <gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+            </LookAt>
+            <Point>
+                <gx:drawOrder>1</gx:drawOrder>
+                <coordinates>{attributes['lon']},{attributes['lat']},0</coordinates>
+            </Point>
+            <atom:link rel="app" href="https://www.google.com/earth/about/versions/#earth-pro" title="Google Earth Pro 7.3.6.10201"></atom:link>
+        </Placemark>'''
